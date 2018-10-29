ab6292fa02e54e236bb5eb0f2461ddf37bcbf13d
ihollander <ianholla@gmail.com>
2018-10-29 18:00:20 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  def self.form_attributes
+    %w(name weight height)
+  end
+
+  self.form_attributes.each{|attribute| self.send(:attr_accessor, attribute) }
+
+  @@all = []
+
+  def initialize(params)
+    self.class.form_attributes.each do |attribute| 
+      self.send("#{attribute}=", params[attribute])
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+  def self.form_attributes
+    %w(name type booty)
+  end
+
+  # RUBY IS CRAZY - send whaaa
+  self.form_attributes.each{|attribute| self.send(:attr_accessor, attribute) }
+
+  @@all = []
+
+  def initialize(params)
+    self.class.form_attributes.each do |attribute| 
+      self.send("#{attribute}=", params[attribute])
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
