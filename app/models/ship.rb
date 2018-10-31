e0f3f5526e89ae0df200e738e643393b624b98b5
aaaaus <ausluft@gmail.com>
2018-10-31 13:06:09 -0400

    lab completed

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
-end
+
+  @@all = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(args)
+    @name = args["name"]
+    @type = args["type"]
+    @booty = args["booty"]
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+end #class Ship
